fffd94daf97f1549498c1efa237fe6187ba9bfdf
Mar Masó <masogm@campusemav.com>
2020-10-05 11:04:25 +0200

    DIa 3
    Fins al controlador player

---
+++ Assets/Scripts/ScrPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPickup : MonoBehaviour
+{
+    [SerializeField]
+    int velGir = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, velGir * Time.deltaTime);
+      
+    }
+}
---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float velocitat = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y; // per llegir gamepad
+        x = Input.GetAxis("Horizontal"); // llegim moviment horizontal
+        y = Input.GetAxis("Vertical"); //llegim moviment vertical
+        // print("Horizontal : " + x + " / Vertical: " + y); //mostrem x e y
+        transform.Translate(x *Time.deltaTime*velocitat, y*Time.deltaTime*velocitat, 0);
+
+    }
+}
