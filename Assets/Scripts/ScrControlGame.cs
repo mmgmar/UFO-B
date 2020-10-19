c56e2b936e081cc1fea26e5cb42ab324453c6e46
Mar Masó <masogm@campusemav.com>
2020-10-19 09:58:59 +0200

    Classe 19/10
    Fins puntuació pickups

---
+++ Assets/Scripts/ScrControlGame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrControlGame : MonoBehaviour
+{
+    public static int punts = 0; // puntuació
+
+}
--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -4,22 +4,50 @@ using UnityEngine;
 
 public class ScrPlayer : MonoBehaviour
 {
+    float x, y; // variables per guardar lectura dels cursos
+
     [SerializeField]
-    float velocitat = 3;
+    float forsa = 3;
+    // Per accedir al component RigidBody:
+    Rigidbody2D rb; 
+
+    // [SerializeField]
+    // float velocitat = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // ara rb apunta al component RigidBody2D del player
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x, y; // per llegir gamepad
+        // float x, y; // per llegir gamepad
         x = Input.GetAxis("Horizontal"); // llegim moviment horizontal
         y = Input.GetAxis("Vertical"); //llegim moviment vertical
+        
+
         // print("Horizontal : " + x + " / Vertical: " + y); //mostrem x e y
-        transform.Translate(x *Time.deltaTime*velocitat, y*Time.deltaTime*velocitat, 0);
+       // transform.Translate(x *Time.deltaTime*velocitat, y*Time.deltaTime*velocitat, 0);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x*forsa, y*forsa));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // print(collision.name);
+        // if (collision.tag == "pickup") Destroy(collision.gameObject);
+        // if (collision.CompareTag("pickup")) Destroy(collision.gameObject);
+        if (collision.CompareTag("pickup"))
+        {
+            Destroy(collision.gameObject);
+            ScrControlGame.punts += 5;
+            print("Puntuació: " + ScrControlGame.punts);
+        }
 
     }
 }
